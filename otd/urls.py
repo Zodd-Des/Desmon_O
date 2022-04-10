8c093cce65b30ae91dfba050cf1b5908d320d0b2
Zodd-Des <62941328+Zodd-Des@users.noreply.github.com>
2022-04-10 15:19:04 +0300

    added domain name

--- otd/urls.py
+++ otd/urls.py
@@ -18,12 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns = [
     path('', include('contact.urls')),
     path('admin/', admin.site.urls),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
 ]
